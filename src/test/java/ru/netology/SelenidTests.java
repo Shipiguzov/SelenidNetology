a9e984d52fd5e3a98fbd260a9002f219f3512928
Thorn <tibalt84@gmail.com>
2022-01-30 15:12:39 +0300

    Initial commit

---
+++ src/test/java/ru/netology/SelenidTests.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenidTests {
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        open("http://localhost:9999/");
+    }
+}
